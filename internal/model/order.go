ba286a1d0f773b8910514e327567b106f9471d28
lucoscly <lucoscly@student.21-school.ru>
2025-06-30 14:37:29 +0300

    fix: edit Order struct to contain delivery, payment and items

--- internal/model/order.go
+++ internal/model/order.go
@@ -6,6 +6,9 @@ type Order struct {
 	OrderUID    string `json:"order_uid"`
 	TrackNumber string `json:"track_number"`
 	Entry       string `json:"entry"`
+	Delivery Delivery `json:"delivery"`
+	Payment Payment `json:"payment"`
+	Items []Item `json:"items"`
 	Locale      string `json:"locale"`
 	InternalSignature string `json:"internal_signature"`
 	CustomerID string `json:"customer_id"`
@@ -14,6 +17,8 @@ type Order struct {
 	SmID int `json:"sm_id"`
 	DateCreated time.Time `json:"date_created"`
 	OofShard string `json:"oof_shard"`
+
+	Del
 }
 
 
@@ -36,6 +41,10 @@ type Payment struct {
 	Amount int `json:"amount"`
 	PaymentDT int `json:"payment_dt"`
 	Bank string `json:"bank"`
+	DeliveryCost int `json:"delivery_cost"`
+	GoodsTotal int `json:"goods_total"`
+	CustomFee int `json:"custom_fee"`
+
 }
 
 type Item struct {

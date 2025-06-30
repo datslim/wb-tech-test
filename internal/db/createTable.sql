9749715fe757b1ab29c7bb2bdf100432ef65bd9b
lucoscly <lucoscly@student.21-school.ru>
2025-06-30 13:00:49 +0300

    feat: add model structs for db

---
+++ internal/db/createTable.sql
@@ -0,0 +1,54 @@
+CREATE TABLE orders (
+    order_uid VARCHAR PRIMARY KEY,
+    track_number VARCHAR,
+    entry VARCHAR,
+    locale VARCHAR,
+    internal_signature VARCHAR,
+    customer_id VARCHAR,
+    delivery_service VARCHAR,
+    shardkey VARCHAR,
+    sm_id INTEGER,
+    date_created TIMESTAMP,
+    oof_shard VARCHAR
+);
+
+CREATE TABLE delivery (
+    order_uid VARCHAR PRIMARY KEY REFERENCES orders(order_uid),
+    name VARCHAR,
+    phone VARCHAR,
+    zip VARCHAR,
+    city VARCHAR,
+    address VARCHAR,
+    region VARCHAR,
+    email VARCHAR
+);
+
+CREATE TABLE payment (
+    transaction VARCHAR PRIMARY KEY,
+    order_uid VARCHAR REFERENCES orders(order_uid),
+    request_id VARCHAR,
+    currency VARCHAR,
+    provider VARCHAR,
+    amount INTEGER,
+    payment_dt BIGINT,
+    bank VARCHAR,
+    delivery_cost INTEGER,
+    goods_total INTEGER,
+    custom_fee INTEGER
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    order_uid VARCHAR REFERENCES orders(order_uid),
+    chrt_id INTEGER,
+    track_number VARCHAR,
+    price INTEGER,
+    rid VARCHAR,
+    name VARCHAR,
+    sale INTEGER,
+    size VARCHAR,
+    total_price INTEGER,
+    nm_id INTEGER,
+    brand VARCHAR,
+    status INTEGER
+);

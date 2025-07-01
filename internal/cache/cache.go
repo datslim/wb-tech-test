c746ecb1f2da24f2c51fb312c2e70286a564458e
lucoscly <lucoscly@student.21-school.ru>
2025-07-01 13:55:01 +0300

    feat: make GetAll() && Restore() func for cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -26,10 +26,33 @@ func (c *OrderCache) Set(order model.Order) {
 	c.cache[order.OrderUID] = order // добавляем заказ в кеш по ключу orderUID
 }
 
+// восстановление кэша (например, при первичном заполнении)
+func (c *OrderCache) Restore(orders []model.Order) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for _, order := range orders {
+		c.cache[order.OrderUID] = order
+	}
+}
+
 // получение заказа из кеша
+// возвращаемое значение экземпляр типа Order и флаг указывающий на то, существует ли заказ в кеше или нет
 func (c *OrderCache) Get(orderUID string) (model.Order, bool) {
 	c.mu.RLock()                   // блокируем кеш для чтения
 	defer c.mu.RUnlock()           // разблокируем кеш после чтения
 	order, ok := c.cache[orderUID] // получаем заказ из кеша по ключу orderUID
 	return order, ok               // возвращаем заказ и флаг, указывающий, существует ли заказ в кеше (1 - заказ существует, 0 - заказ не существует)
+}
+
+// получение всех заказов из кеша
+// возвращаемое значение: слайс типа Order
+func (c *OrderCache) GetAll() []model.Order {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	orders := make([]model.Order, 0, len(c.cache))
+	for _, order := range c.cache {
+		orders = append(orders, order)
+	}
+
+	return orders
 }

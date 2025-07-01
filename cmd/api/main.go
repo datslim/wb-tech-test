fbea763c052f7c24d7ea05f91358ff1cd3663b19
lucoscly <lucoscly@student.21-school.ru>
2025-07-01 16:11:24 +0300

    refactor: move api server start to cmd folder

---
+++ cmd/api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"wb-tech-test/internal/api"
+	"wb-tech-test/internal/cache"
+	"wb-tech-test/internal/db"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	database := db.NewDB()
+	defer database.Pool.Close()
+
+	ctx := context.Background()
+
+	orders, err := database.GetAllOrders(ctx) // получаем все заказы из БД
+	if err != nil {
+		fmt.Println("Ошибка при получении всех заказов:", err)
+		return
+	}
+	orderCache := cache.NewOrderCache() // создаем новый кэш
+	orderCache.Restore(orders)          // записываем в кэш данные, полученные из БД
+
+	// Создаём и запускаем HTTP-сервер
+	Server := api.NewServer(database, orderCache)
+
+	err = godotenv.Load() // загрузка переменных окружения из файла .env
+	if err != nil {
+		log.Fatalf("Ошибка при загрузке файла .env для API: %v", err)
+	}
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "8081"
+	}
+	log.Printf("API server запущен на порту :%s\n", port)
+	log.Println(Server.Start(":" + port))
+}
